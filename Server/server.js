a3a9e455f859168e46d6891e21db2e0c9d274e0c
VenkataramanTB <98270783+venkataramanTB@users.noreply.github.com>
2024-03-04 08:30:37 +0530

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const { Pool } = require('pg');
 const app = express();
-const port = 3000;
 require('dotenv').config();
 app.use(cors());
 app.use(express.json());
@@ -40,7 +39,7 @@ app.get("/flights", async (req, res) => {
         console.error(err.message);
     }
 });
-
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`App running on port ${port}.`)
 });
